af22857ee3e49753cfb82bf79ca9da9b2b7de17e
Sajid1000 <42867037+Sajid1000@users.noreply.github.com>
2024-01-01 19:53:57 +0530

    Update getRecentKDrama.js

--- routes/getRecentKDrama.js
+++ routes/getRecentKDrama.js
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
                     if (!page) {
                         page = 1
                     }
-                    const siteUrl = `https://asianembed.io/kshow?page=${page}`
+                    const siteUrl = `https://draplay.info/kshow?page=${page}`
                     const { data } = await axios.get(siteUrl)
                     const $ = cheerio.load(data)
                     let dataArr = []
