5a90b852ba84fc48dfa3210b9180b093b8364cd2
Sajid1000 <42867037+Sajid1000@users.noreply.github.com>
2024-01-03 01:47:22 +0530

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -2,20 +2,7 @@ const express = require('express')
 const router = express.Router();
 
 router.get('', (req,res) => {
-    return res.send({response: ' these are the endpoints :
-/api/recent returns recent subbed drama.
-
-/api/recentraw returns recent raw drama.
-
-/api/recentkdrama returns recent kdrama.
-
-/api/popular returns popular drama.
-
-/api/ongoing returns ongoing drama.
-
-/api/movies returns recent movies.
-
-/api/detail/{id} returns details of drama'})
+    return res.send({response: 'Invalid route or missing param/apikey.'})
 })
 
 module.exports = router;
