3fc85b0f5b9ebe86065758a5edd8dc25d6b61abf
Sajid1000 <42867037+Sajid1000@users.noreply.github.com>
2024-01-04 20:53:52 +0530

    Update getEpisodeList.js

--- routes/getEpisodeList.js
+++ routes/getEpisodeList.js
@@ -1,63 +1,81 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
-const express = require('express');
-require('dotenv').config();
+const cheerio = require('cheerio')
+const express = require('express')
+require('dotenv').config()
 
-const apiKey = process.env.API_KEY;
+const apiKey = process.env.API_KEY
 const router = express.Router();
 
 router.get('/:id', async (req, res) => {
-  const api_key = req.query.api_key;
-  
-  if (api_key !== apiKey) {
-    return res.status(403).json({ response: 'Invalid API Key!' });
-  }
-
-  try {
-    const id = req.params.id;
-    const siteUrl = `https://draplay.info/videos/${id}`;
-    const { data } = await axios.get(siteUrl);
-    const $ = cheerio.load(data);
-
-    const episodeList = $("ul.listing.items.lists > li.video-block")
-      .map((i, el) => {
-        const $el = $(el);
-        const title = $el.find("a > div.name").text();
-        const date = $el.find("a > div.meta > span.date").text();
-        const URL = $el.find("a").attr("href");
-        const cover = $el.find("a > div.img > div.picture > img").attr("src");
-        const episodeId = i + 1;
-
-        return {
-          id: episodeId,
-          cover: cover,
-          title: title,
-          date: date,
-          URL: URL,
-        };
-      })
-      .toArray();
-
-    const parentPara = $('div.content-more-js');
-    const checkPara = $(parentPara).find('p');
-    const dataArr = [];
-
-    if (checkPara.length) {
-      let description = '';
-      checkPara.each((index, element) => {
-        description += $(element).text().trim() + '\n';
-      });
-      dataArr.push({ description, episodeList });
-    } else {
-      let description = $(parentPara).text().trim();
-      dataArr.push({ description, episodeList });
+    let api_key = req.query.api_key
+    if (api_key != apiKey) {
+        return res.send({response: 'Invalid API Key!'})
     }
+    else {
+        try {
+            const getEpisodeList = async () => {
+                try {
+                    let id = req.params.id;
+                    const siteUrl = `https://draplay.info/videos/${id}`
+                    const { data } = await axios.get(siteUrl)
+                    const $ = cheerio.load(data)
+                    let dataArr = []
+                    
 
-    return res.status(200).json({ result: dataArr });
-  } catch (error) {
-    console.error('An error occurred:', error);
-    return res.status(500).json({ error: error.toString() });
-  }
-});
+                    const episodeList = $("ul.listing.items.lists > li.video-block")
+        .map((i, el) => {
+          const $el = $(el);
+          const title = $el.find("a > div.name").text();
+          const date = $el.find("a > div.meta > span.date").text();
+          const URL = $el.find("a").attr("href");
+          const cover = $el.find("a > div.img > div.picture > img").attr("src");
+          const id = i + 1;
+          return {
+            id: id,
+            cover: cover,
+            title: title,
+            date: date,
+            URL: URL,
+          };
+        })
+        .toArray();
+                    
+                    const parentPara = $('div.content-more-js')
+                    const checkPara = $(parentPara).find('p')
+            
+                    if (checkPara.length) {
+                        let description = ''
+                        checkPara.each((index, element) => {
+                            description += $(element).text().trim() + '\n'
+                        })
+                        dataArr.push({
+                           
+                            episodeList
+                        })
+                    }
+                    else {
+                        let description = $(parentPara).text().trim()
+                        dataArr.push({
+                           
+                            episodeList
+                        })
+                    }
+                    return dataArr
+                }
+                catch (error) {
+                    console.error(error)
+                }
+            }
+            const apiData = await getEpisodeList()
+            return res.status(200).json({
+                result: apiData,
+            })
+        } catch (error) {
+            return res.status(500).json({
+                error: error.toString(),
+            })
+        }
+    }
+})
 
-module.exports = router;
+module.exports = router;  
