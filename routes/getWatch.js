5bba711e2d0d9e860d7ca669188e3b70d002eb6d
Sajid1000 <42867037+Sajid1000@users.noreply.github.com>
2024-03-16 02:01:42 +0530

    Update getWatch.js

--- routes/getWatch.js
+++ routes/getWatch.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { JSDOM } from 'jsdom'
-import { generateEncryptAjaxParameters, decryptEncryptAjaxResponse } from '../extractors/asianload.js'
+import { generateEncryptAjaxParameters, decryptEncryptAjaxResponse } from './extractors/asianload.js'
 
 const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0 Win64 x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
 
